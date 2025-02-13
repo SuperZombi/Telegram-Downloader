46e6f9528fc6ab998cb715100abcf85e8939e873
Super Zombi <super.zombi.yt@gmail.com>
2025-02-13 22:07:45 +0200

    Fix k changed icon

--- src/main/tg_downloader_k.js
+++ src/main/tg_downloader_k.js
@@ -3,13 +3,15 @@ else{
 	var TG_DOWNLOADER_EXECUTED = true;
 
 
-var DOWNLOAD_ICON = "\uE94E";
+var DOWNLOAD_ICON = 59738;
 onAppear(".media-viewer-aspecter", aspecter => {
 	let mediaContainer = aspecter.closest(".media-viewer-whole")
 	let mediaButtons = mediaContainer.querySelector(".media-viewer-topbar .media-viewer-buttons")
-	let hiddenButtons = mediaButtons.querySelectorAll("button.btn-icon.hide");
+	let hiddenButtons = mediaButtons.querySelectorAll("button.btn-icon.hide")
 	for (let btn of hiddenButtons) {
-		if (btn.textContent === DOWNLOAD_ICON) {
+		console.log(btn.textContent)
+		console.log(btn.textContent === DOWNLOAD_ICON)
+		if (btn.textContent.charCodeAt(0) === DOWNLOAD_ICON) {
 			btn.classList.remove("hide");
 			return
 		}

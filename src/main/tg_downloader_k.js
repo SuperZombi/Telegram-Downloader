9db0392711829aa799f8b1c8008dbce7f94023c6
mawngo <sitdownrightnow.dev@gmail.com>
2026-03-13 19:41:08 +0700

    Fix download button detection on /k/

--- src/main/tg_downloader_k.js
+++ src/main/tg_downloader_k.js
@@ -3,20 +3,12 @@ else{
 	var TG_DOWNLOADER_EXECUTED = true;
 
 
-var DOWNLOAD_ICON = 59738;
 onAppear(".media-viewer-aspecter", aspecter => {
 	let mediaContainer = aspecter.closest(".media-viewer-whole")
 	let mediaButtons = mediaContainer.querySelector(".media-viewer-topbar .media-viewer-buttons")
-	let hiddenButtons = mediaButtons.querySelectorAll("button.btn-icon.hide")
-	for (let btn of hiddenButtons) {
-		console.log(btn.textContent)
-		console.log(btn.textContent === DOWNLOAD_ICON)
-		if (btn.textContent.charCodeAt(0) === DOWNLOAD_ICON) {
-			btn.classList.remove("hide");
-			return
-		}
-	}
+    let downloadButton = mediaButtons.querySelector("button.btn-icon.quality-download-options-button-menu.hide")
+    if (downloadButton) {
+      downloadButton.classList.remove("hide");
+    }
 })
-
-
 }

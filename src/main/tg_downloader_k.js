7e4b6bc7cd7f004d02737e101415010c90138169
Super Zombi <super.zombi.yt@gmail.com>
2026-03-14 14:31:28 +0000

    1.0.3

--- src/main/tg_downloader_k.js
+++ src/main/tg_downloader_k.js
@@ -2,13 +2,12 @@ if (TG_DOWNLOADER_EXECUTED){}
 else{
 	var TG_DOWNLOADER_EXECUTED = true;
 
-
 onAppear(".media-viewer-aspecter", aspecter => {
 	let mediaContainer = aspecter.closest(".media-viewer-whole")
 	let mediaButtons = mediaContainer.querySelector(".media-viewer-topbar .media-viewer-buttons")
-    let downloadButton = mediaButtons.querySelector("button.btn-icon.quality-download-options-button-menu.hide")
-    if (downloadButton) {
-      downloadButton.classList.remove("hide");
-    }
+	let downloadButton = mediaButtons.querySelector("button.btn-icon.quality-download-options-button-menu.hide")
+	if (downloadButton) {
+		downloadButton.classList.remove("hide");
+	}
 })
 }
